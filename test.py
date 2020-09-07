0f5c26632bf8f790bee5c71b3f63d13dfe4aa819
alibava <alibava@localhost.localdomain>
2020-09-07 15:48:40 +0800

    test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import pyvisa
 rm = pyvisa.ResourceManager()
 print(rm.list_resources())
-print(rm.list_resources()[0])
-inst = rm.open_resource(rm.list_resources()[0])
-print(inst.query("*IDN?"))
+if len(rm.list_resources()) != 0:
+    print(rm.list_resources()[0])
+    inst = rm.open_resource(rm.list_resources()[0])
+    print(inst.query("*IDN?"))
